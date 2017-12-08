4e6be8cdcd8d71a437c2b04676e540b03743eb6c
Kebtarum <33719160+Kebtarum@users.noreply.github.com>
2017-12-08 21:19:23 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,56 +1,80 @@
-var model = {
-    items: [{
-		date:"-",
-		purchase:"-",
-		price:"-", 
-		}
-    ]
-};
+//var model = {
+  //  items: [{
+	//	date:"-",
+	//	purchase:"-",
+	//	price:"-", 
+	//	}
+   // ]
+//};
 
-
-
+var modelFather = {items:[], 	
+	RUR : 0,
+	EUR : 0,
+	USD : 0,};
+var modelMother = {items:[],
+	RUR : 0,
+	EUR : 0,
+	USD : 0};
+var selectFilter = "{price: '-'}";
 var myApp = angular.module("myApp", []);
 
 myApp.controller("pController", function ($scope) {
-	$scope.RUR = 0;
-	$scope.EUR = 0;
-	$scope.USD = 0;
-    $scope.list = model;
-    $scope.addItem = function (date ,text, price,currency) {
+	//$scope.RUR = 0;
+	//$scope.EUR = 0;
+	//$scope.USD = 0;
+    $scope.list = modelFather;
+
+	$scope.changeList = function(modelFP){
+		if(modelFP == 1)
+			$scope.list = modelFather;
+		
+		else
+			$scope.list = modelMother;
+	};
+	
+	
+    $scope.addItem = function (ThisIsData ,text, price,currency) {
        // price = parseFloat(price); // преобразуем введенное значение к числу
 		if(price[0] == '+' | price[0] == '-'){
+			alert(price);
 			var buf = price[0];
 			price = price.slice(1);
 			price = parseFloat(price);
-			if(text != "" && !isNaN(parseFloat(price)) && date != undefined && currency != undefined) // если текст установлен и введено число, то добавляем
+			alert(price);
+			
+			var DATA = ThisIsData.toLocaleDateString();
+			
+			if(text != "" && !isNaN(parseFloat(price)) && DATA != undefined && currency != undefined) // если текст установлен и введено число, то добавляем
 			{
-				$scope.list.items.push({date: date, purchase: text, price: buf + price + ' ' + currency});
+				$scope.list.items.push({date: DATA, purchase: text, price: buf + price + ' ' + currency});
+				alert(price);
 				switch(currency){
 					case "RUR": 
 						if(buf == '+'){
-							$scope.RUR = $scope.RUR + +price
+							$scope.list.RUR = $scope.list.RUR + +price
+							alert($scope.list.RUR);
 						}
 						else{
-							$scope.RUR = $scope.RUR - +price;
+							$scope.list.RUR = $scope.list.RUR - +price;
 						}
 						break;
 					case "EUR": 
 						if(buf == '+'){
-							$scope.EUR = $scope.EUR + +price
+							$scope.list.EUR = $scope.list.EUR + +price
 						}
 						else{
-							$scope.EUR = $scope.EUR - +price;
+							$scope.list.EUR = $scope.list.EUR - +price;
 						}
 						break;
 					case "USD":
 						if(buf == '+'){
-							$scope.USD = $scope.USD + +price
+							$scope.list.USD = $scope.list.USD + +price
 						}
 						else{
-							$scope.USD = $scope.USD - +price;
+							$scope.list.USD = $scope.list.USD - +price;
 						}
 						break;
-					default: ;	
+					default: break;	
 			}
 		}
 	}
