8b4fa06c4e5121dad64ddc122679aeb67b836070
Kebtarum <33719160+Kebtarum@users.noreply.github.com>
2017-12-10 19:06:15 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-//var model = {
-  //  items: [{
-	//	date:"-",
-	//	purchase:"-",
-	//	price:"-", 
-	//	}
-   // ]
-//};
+var Models = [];
+var i = 0
 
-var modelFather = {items:[], 	
+var modelFather = {name: "Не выбран",items:[], 	
 	RUR : 0,
 	EUR : 0,
 	USD : 0,};
-var modelMother = {items:[],
-	RUR : 0,
-	EUR : 0,
-	USD : 0};
+//var modelMother = {name: "Второй" ,items:[],
+	//RUR : 0,
+	//EUR : 0,
+	//USD : 0};
 var selectFilter = "{price: '-'}";
 var myApp = angular.module("myApp", []);
 
@@ -22,32 +16,55 @@ myApp.controller("pController", function ($scope) {
 	//$scope.RUR = 0;
 	//$scope.EUR = 0;
 	//$scope.USD = 0;
+	$scope.AllList = Models;
     $scope.list = modelFather;
-
-	$scope.changeList = function(modelFP){
-		if(modelFP == 1)
-			$scope.list = modelFather;
+	
+	$scope.AddModels = function(nameModel){
 		
-		else
-			$scope.list = modelMother;
+		$scope.AllList.push({number: i, name: nameModel, items:[],
+		RUR : 0,
+		EUR : 0,
+		USD : 0
+		});
+		i++;
+		//alert ($scope.AllList);
+		//alert (Models.name);
+	};
+	
+	$scope.DelModels = function(modelNumber){
+		alert(modelNumber);
+		$scope.AllList.splice(modelNumber,1);
+		
+		while(modelNumber  < $scope.AllList.length){
+			$scope.AllList[modelNumber].number -= 1;
+			modelNumber++;
+		}
+		--i
+		//$scope.AllList.splice(number,number);
+	};
+	
+	$scope.changeList = function(modelFP){
+		$scope.list = modelFP;
+		//if(modelFP == 1)
+			//$scope.list = modelFather;
+		//else
+			//$scope.list = modelMother;
+		alert("Привет " + $scope.list.name);
 	};
 	
 	
     $scope.addItem = function (ThisIsData ,text, price,currency) {
        // price = parseFloat(price); // преобразуем введенное значение к числу
 		if(price[0] == '+' | price[0] == '-'){
-			alert(price);
 			var buf = price[0];
 			price = price.slice(1);
 			price = parseFloat(price);
-			alert(price);
 			
 			var DATA = ThisIsData.toLocaleDateString();
 			
 			if(text != "" && !isNaN(parseFloat(price)) && DATA != undefined && currency != undefined) // если текст установлен и введено число, то добавляем
 			{
 				$scope.list.items.push({date: DATA, purchase: text, price: buf + price + ' ' + currency});
-				alert(price);
 				switch(currency){
 					case "RUR": 
 						if(buf == '+'){
